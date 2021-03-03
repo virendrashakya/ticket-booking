fe8e1dacab381bb65484a90d2443d9ad485236fa
Virendra Shakya <virendrashakya@Virendras-MacBook-Air.local>
2021-03-03 12:36:59 +0530

    login signup page added

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,8 +5,17 @@ class OrdersController < ApplicationController
     # binding.pry
     o = Order.new(user_id: current_user.id, seat_ids: params[:seat_id], show_id: params[:show_id], price: total_price)
     ActiveRecord::Base.transaction do
-      o.save!
-      seats.update(order_id: o.id)
+      if o.save
+        seats.update(order_id: o.id)
+        flash[:success] = "Woohoo!"
+        render 'show'
+      else
+        flash[:alert] = "Alerting you to the monkey on your car!"
+      end
     end
+  end
+
+  def show
+
   end
 end
